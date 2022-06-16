fb0ce49901cb1928488f959cd83e8ee66596af0c
Antonio Casanova Junior da Silva Lima <casanovajr25@gmail.com>
2022-06-16 17:30:22 -0300

    feat: Create Css for add-task

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import Tasks from "./components/Tasks";
 import "./App.css";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   // const message = "Hello World!";
@@ -21,6 +22,7 @@ const App = () => {
   return (
     <>
       <div className="container"></div>
+      <AddTask />
       <Tasks tasks={tasks} />
     </>
   );
